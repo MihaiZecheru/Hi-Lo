1f928a48996e0c44bf8d4f9133efaac85ed541f1
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2022-05-22 01:30:06 -0700

    Revert "Optimized the DefaultColor property"
    This reverts commit 938593df66150924de7ed70b0d652d4ee6dc53a6.

--- Data/User.cs
+++ Data/User.cs
@@ -8,8 +8,6 @@ namespace Arcade
 {
     public class User
     {
-        private static string dl = "------------------------------------------------------------------------------------------------------------------------";
-
         public int id { get; }
         public string name { get; set; }
         public string password { get; set; }
@@ -90,207 +88,51 @@ namespace Arcade
 
         public static async Task<User> SignIn()
         {
-            string space = "";
-            for (int i = 0; i < 55; i++) space += ' ';
+            string dl = "-------------------------------------------------------------";
+            string space = "                            ";
 
-            /* ask for sign-in or sign-up;
-             * 1 is sign-in
-             * 2 is sign-up
-            */
-            int option = GetSignInOrSignUpOption();
+            Arcade.ConsoleColors.Set("cyan");
+            Console.WriteLine($"{dl}\n\n{space}Sign In");
 
-            if (option == 1)
-            {
-                bool first = true;
-                int id;
-                do
-                {
-                    if (!first)
-                    {
-                        Thread.Sleep(2000);
-                        Console.Clear();
-                    }
-                    first = false;
-
-                    Arcade.ConsoleColors.Set("cyan");
-                    Console.WriteLine($"{dl}\n\n{space}Sign In");
-
-                    Arcade.ConsoleColors.Set("cyan");
-                    Console.Write($"\n{dl}\n\nUsername: ");
-                    Arcade.ConsoleColors.Set("magenta");
-                    string username = Console.ReadLine();
-                    Arcade.ConsoleColors.Set("cyan");
-                    Console.Write("Password: ");
-                    Arcade.ConsoleColors.Set("magenta");
-                    string password = Console.ReadLine();
-
-                    Console.Write("\n");
-
-                    Arcade.ConsoleColors.Set("cyan");
-                    DotAnimation dotAnimationChecking = new DotAnimation("Looking for your account");
-                    id = await Arcade.Database.DoesUserExist(username, password);
-                    if (id == 0)
-                    {
-                        dotAnimationChecking.End("This account does not exist", "darkred");
-                        Arcade.ConsoleColors.Set("cyan");
-                        Console.WriteLine('\n' + dl);
-                    }
-                    else
-                    {
-                        dotAnimationChecking.End("Found it!", "magenta");
-                        Arcade.ConsoleColors.Set("cyan");
-                    }
-                } while (id == 0);
-
-                DotAnimation dotAnimationGetting = new DotAnimation("Signing in", "Done!\n");
-                User user = await Arcade.Database.GetUser(id);
-                Thread.Sleep(2000);
-                dotAnimationGetting.End(consoleColor: "magenta");
-                Arcade.ConsoleColors.Set("cyan");
-                Console.WriteLine(dl);
-                Arcade.ConsoleColors.Reset();
-                Thread.Sleep(1250);
-                Console.Clear();
-                return user;
-            }
-            else // option == 2
-            {
-                bool first = true;
-                string username;
-                string password = "placeholder";
-                string confirm_password = "placeholder";
-                while (true)
-                {
-                    Arcade.ConsoleColors.Set("cyan");
-                    if (!first)
-                    {
-                        Thread.Sleep(2000);
-                        Console.Clear();
-                    }
-                    
-                    first = false;
-                    Console.WriteLine($"{dl}\n\n{space}Sign Up\n");
-                    Console.Write($"{dl}\n\nCreate Your Username: ");
-                    Arcade.ConsoleColors.Set("magenta");
-                    username = Console.ReadLine();
-
-                    Arcade.ConsoleColors.Set("cyan");
-                    Console.Write($"\n{dl}\n\nDo you want to choose a different username? (y/n): ");
-                    Arcade.ConsoleColors.Set("magenta");
-                    string response = Console.ReadLine();
-                    if (response == "y" || response == "yes")
-                        continue;
-
-                    Arcade.ConsoleColors.Set("cyan");
-                    first = true;
-                    Thread.Sleep(1000);
-                    Console.Clear();
-
-                    while (true)
-                    {
-
-                        if (!first)
-                        {
-                            Thread.Sleep(1500);
-                            Console.Clear();
-                        }
-
-                        first = false;
-
-                        Console.WriteLine($"{dl}\n\n{space}Sign Up\n");
-                        Arcade.ConsoleColors.Set("cyan");
-                        Console.Write($"{dl}\n\nCreate Your Password: ");
-                        Arcade.ConsoleColors.Set("magenta");
-                        password = Console.ReadLine().Trim(' ');
-
-                        Arcade.ConsoleColors.Set("cyan");
-                        Console.Write("\nConfirm Your Password: ");
-                        Arcade.ConsoleColors.Set("magenta");
-                        confirm_password = Console.ReadLine().Trim(' ');
-                        Arcade.ConsoleColors.Set("cyan");
-
-                        string error = "";
-                        if (password != confirm_password)
-                            error = "Passwords must match";
-                        else if (password == "")
-                            error = "Your password cannot be \"\"";
-                        else if (password.Length < 8)
-                            error = "Your password must be 8 characters or longer";
-
-                        if (error != "")
-                        {
-                            Console.Write($"{dl}\n\n");
-                            Arcade.ConsoleColors.Set("darkred");
-                            Console.Write(error);
-                            Arcade.ConsoleColors.Set("cyan");
-                            Console.Write($"\n\n{dl}\n\n");
-                        }
-
-                        if (password == confirm_password && password.Length >= 8 && password != "") break;
-                    }
-                    break;
-                }
-                Console.WriteLine($"\n{dl}\n");
-
-                ConsoleColors.Reset();
-                User user = await Arcade.Database.GetUser(8);
-                return user;
-            }
-        }
-
-        private static int GetSignInOrSignUpOption()
-        {
-            /* Message that will be displayed:
-             * You must either SIGN IN or SIGN UP to play this game!
-
-             * Type 1 to Sign-In to an already existing account.
-             * Type 2 to Sign-Up for an Arcade account. Choose an option:
-            */
-
-            bool passed;
-            int option;
-
-            bool first = true;
+            int id = 0;
             do
             {
-                if (!first)
+                Arcade.ConsoleColors.Set("cyan");
+                Console.Write($"\n{dl}\n\nUsername: ");
+                Arcade.ConsoleColors.Set("green");
+                string username = Console.ReadLine();
+                Arcade.ConsoleColors.Set("cyan");
+                Console.Write("Password: ");
+                Arcade.ConsoleColors.Set("green");
+                string password = Console.ReadLine();
+
+                Console.Write("\n");
+
+                Arcade.ConsoleColors.Set("yellow");
+                DotAnimation dotAnimationChecking = new DotAnimation("Looking for your account");
+                id = await Arcade.Database.DoesUserExist(username, password);
+                if (id == 0)
                 {
-                    Thread.Sleep(1000);
-                    Console.Clear();
+                    dotAnimationChecking.End("This account does not exist", "red");
+                    Arcade.ConsoleColors.Set("yellow");
                 }
-                first = false;
+                else
+                {
+                    dotAnimationChecking.End("Found it!", "green");
+                    Arcade.ConsoleColors.Set("yellow");
+                }
+            } while (id == 0);
 
-                Arcade.ConsoleColors.Set("cyan");
-                Console.WriteLine(dl + "\n");
-                Console.Write("You must either ");
-                Arcade.ConsoleColors.Set("magenta");
-                Console.Write("SIGN IN ");
-                Arcade.ConsoleColors.Set("cyan");
-                Console.Write("or ");
-                Arcade.ConsoleColors.Set("magenta");
-                Console.Write("SIGN UP ");
-                Arcade.ConsoleColors.Set("cyan");
-                Console.Write("to play this game!\n\nType ");
-                Arcade.ConsoleColors.Set("magenta");
-                Console.Write("1 ");
-                Arcade.ConsoleColors.Set("cyan");
-                Console.WriteLine("to Sign-In to an already existing account.");
-                Console.Write("Type ");
-                Arcade.ConsoleColors.Set("magenta");
-                Console.Write("2 ");
-                Arcade.ConsoleColors.Set("cyan");
-                Console.Write("to Sign-Up for an Arcade account. Choose an option: ");
-                Arcade.ConsoleColors.Set("magenta");
-                
-                string response = Console.ReadLine();
-                passed = int.TryParse(response, out option);
-                Arcade.ConsoleColors.Set("cyan");
-                if (!passed) Console.WriteLine("\n" + dl + "\n");
-                
-            } while (!passed || (option != 1 && option != 2));
-
+            DotAnimation dotAnimationGetting = new DotAnimation("Signing in", "Done!\n");
+            User user = await Arcade.Database.GetUser(id);
+            Thread.Sleep(3000);
+            dotAnimationGetting.End(consoleColor: "green");
+            Arcade.ConsoleColors.Set("cyan");
+            Console.WriteLine(dl);
+            Arcade.ConsoleColors.Reset();
+            Thread.Sleep(1250);
             Console.Clear();
-            return option;
+            return user;
         }
     }
 }
