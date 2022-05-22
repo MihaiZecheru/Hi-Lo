6a49eaa917bfcd816e5146ddfea868b43efc301e
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2022-05-22 14:49:16 -0700

    Set up the default template for a game in this series

--- Arcade/HiLo.cs
+++ Arcade/HiLo.cs
@@ -5,23 +5,27 @@ namespace HiLo
     public class Game
     {
         private static string dl { get; } = "------------------------------------------------------------------------------------------------------------------------";
-        private static string HelpMessage { get; } = 
-            "Hi-Lo (pronounced \"High-Low\") is a game where you try to guess if the card succeeding the current card will be HIGHER\nor LOWER than your current card" +
-            "\n\nPlaying Card Points:\nFace cards hold face value, Ace = 1, Jack = 12, Queen = 13, King = 14\n";
 
         public static async Task Main(string[] args)
         {
             Console.Title = "Arcade: Hi-Lo";
 
-            Arcade.ConsoleColors.ChangeDefaultColor("cyan"); Arcade.ConsoleColors.Reset();
-            
-            Console.WriteLine($"{dl}\n\n{HelpMessage}\n{dl}"); Arcade.ConsoleColors.Reset();
-            
-            Console.ReadLine().Trim(' ');
-            Console.Clear();
+            Arcade.ConsoleColors.ChangeDefaultColor("cyan");   
+            ShowHelpMessage();
 
             User user = await Arcade.User.SignIn();
-            user.Print();
+
+            double bet = await user.GetBet();
+            Console.Clear();
         }
-    }
+
+        private static void ShowHelpMessage()
+        {
+            string helpMessage = "Hi-Lo (pronounced \"High-Low\") is a game where you try to guess if the card succeeding the current card will be HIGHER\nor LOWER than your current card" +
+            "\n\nPlaying Card Points:\nFace cards hold face value, Ace = 1, Jack = 12, Queen = 13, King = 14\n";
+            Arcade.ConsoleColors.Reset(); Console.WriteLine($"{dl}\n\n{helpMessage}\n{dl}");
+            Arcade.ConsoleColors.Set("white"); Console.ReadLine().Trim(' ');
+            Arcade.ConsoleColors.Reset(); Console.Clear();
+        }
+    }   
 }
