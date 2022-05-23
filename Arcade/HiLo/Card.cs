d8d86bc0b9b3a7b8381a8ea7095f73710a8a8fc8
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2022-05-22 18:48:22 -0700

    Setup the card class

--- Arcade/HiLo/Card.cs
+++ Arcade/HiLo/Card.cs
@@ -8,8 +8,20 @@ namespace Arcade.HiLo
 {
     internal class Card
     {
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public string Model { get; set; }
+
         public Card()
         {
+            int number;
+            do
+            {
+                Random random = new Random();
+                number = random.Next(1, 15); // cards between 1 and 14 inclusive
+            } while (number != 11);
+
+            
 
         }
     }
