9f75d9618faf887755bab13af27fc57c550aeff4
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2022-05-22 20:42:30 -0700

    Added documentation

--- Arcade/GameTemplate.cs
+++ Arcade/GameTemplate.cs
@@ -1,30 +1,40 @@
-﻿/*
-using Arcade.Template;
+﻿// Replace all occurances of the word "Template" with the name of the game
 
-namespace Template
+/*
+namespace Arcade.Template
 {
     public class Game
     {
+        /// <summary>
+        /// Dotted line
+        /// </summary>
         private static string dl { get; } = "------------------------------------------------------------------------------------------------------------------------";
 
         public static async Task Main(string[] args)
         {
-            double bet = await Initialize();
+            (User user, double bet) = await Initialize();
 
-            StartGame(bet);
+            StartGame(user, bet);
         }
 
+        /// <summary>
+        /// Shows the help message for Template
+        /// </summary>
         private static void ShowHelpMessage()
         {
-            string helpMessage = "";
+            string helpMessage = "Template HelpMessage";
             Arcade.ConsoleColors.Set("cyan"); Console.WriteLine($"{dl}\n\n{helpMessage}\n{dl}");
             Arcade.ConsoleColors.Set("white"); Console.ReadLine().Trim(' ');
             Arcade.ConsoleColors.Set("cyan"); Console.Clear();
         }
 
-        private static async Task<double> Initialize()
+        /// <summary>
+        /// Setup the game by signing the <see cref="User"/> in and getting their bet
+        /// </summary>
+        /// <returns><see cref="Tuple{T1, T2}"/> containing the <see cref="User"/> and the <see cref="User"/>'s bet on the game</returns>
+        private static async Task<Tuple<User, double>> Initialize()
         {
-            Console.Title = "Template";
+            Console.Title = "Arcade: Template";
 
             ShowHelpMessage();
 
@@ -33,10 +43,14 @@ namespace Template
             double bet = await user.GetBet();
             Console.Clear();
             Arcade.ConsoleColors.Reset();
-            return bet;
+            return new Tuple<User, double>(user, bet);
         }
 
-        public static void StartGame(double bet)
+        /// <summary>
+        /// The Template Game
+        /// </summary>
+        /// <param name="bet">The <see cref="User"/>'s bet</param>
+        public static void StartGame(User user, double bet)
         {
             
         }
