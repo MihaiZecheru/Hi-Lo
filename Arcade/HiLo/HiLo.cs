12d966ed5ce6298fa4fd7654127fa624ad01ff51
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2022-05-22 18:07:17 -0700

    Moved HiLo to its own directory

--- Arcade/HiLo/HiLo.cs
+++ Arcade/HiLo/HiLo.cs
@@ -17,28 +17,28 @@ namespace HiLo
         {
             string helpMessage = "Hi-Lo (pronounced \"High-Low\") is a game where you try to guess if the card succeeding the current card will be HIGHER\nor LOWER than your current card" +
             "\n\nPlaying Card Points:\nFace cards hold face value, Ace = 1, Jack = 12, Queen = 13, King = 14\n";
-            Arcade.ConsoleColors.Reset(); Console.WriteLine($"{dl}\n\n{helpMessage}\n{dl}");
+            Arcade.ConsoleColors.Set("cyan"); Console.WriteLine($"{dl}\n\n{helpMessage}\n{dl}");
             Arcade.ConsoleColors.Set("white"); Console.ReadLine().Trim(' ');
-            Arcade.ConsoleColors.Reset(); Console.Clear();
+            Arcade.ConsoleColors.Set("cyan"); Console.Clear();
         }
 
         private static async Task<double> Initialize()
         {
             Console.Title = "Arcade: Hi-Lo";
 
-            Arcade.ConsoleColors.ChangeDefaultColor("cyan");
             ShowHelpMessage();
 
             User user = await Arcade.User.SignIn();
 
             double bet = await user.GetBet();
             Console.Clear();
+            Arcade.ConsoleColors.Reset();
             return bet;
         }
 
         public static void StartGame(double bet)
         {
-
+            
         }
-    }   
+    }
 }
